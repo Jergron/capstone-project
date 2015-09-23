b2ce340690c012fb36c878042320fd6f6b0a6728
Jeremy <jeremygrondahl@gmail.com>
2015-09-23 11:35:44 -0500

    Added another condition in bandprofile.html to help restrict data from populating fan feedback in different band profiles.

--- app/controllers/BandProCtrl.js
+++ app/controllers/BandProCtrl.js
@@ -44,58 +44,6 @@ app.controller("BandProCtrl",
 
     $scope.previewFile();  
 
-    $(function () {
-      $("#f_elem_city").autocomplete({
-        source: function (request, response) {
-          $.getJSON(
-          "http://gd.geobytes.com/AutoCompleteCity?callback=?&filter=US&template=<geobytes%20city>,%20<geobytes%20code>&q=" + request.term,
-          function (data) {
-           response(data);
-          }
-         );
-        },
-        minLength: 3,
-        select: function (event, ui) {
-         var selectedObj = ui.item;
-         $("#f_elem_city").val(selectedObj.value);
-        getcitydetails(selectedObj.value);
-         return false;
-        },
-        open: function () {
-         $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
-        },
-        close: function () {
-         $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
-        }
-      });
-      $("#f_elem_city").autocomplete("option", "delay", 100);
-    });
-
-    function getcitydetails(fqcn) {
-  
-
-      fqcn = $("#f_elem_city").val();
-      console.log("fqcn", fqcn);
-
-      cityfqcn = fqcn;
-
-      if (cityfqcn) {
-
-        $.getJSON("http://gd.geobytes.com/GetCityDetails?callback=?&fqcn="+cityfqcn).then(
-          function (data) {
-            
-            console.log("data", data);
-            $("#geobytesregionlocationcode").val(data.geobytesregionlocationcode);
-            $("#geobytesregion").val(data.geobytesregion);
-            console.log("#geobytesreqionlocationcode", $("#geobytesregionlocationcode").val(data.geobytesregionlocationcode));
-            $scope.userDetails.city = data.geobytescity;
-            $scope.userDetails.state = data.geobytesregion;
-    
-          }
-        );
-      }
-      
-    }
   
     $scope.updateUser = function () {
       var baseRef = new Firebase("https://testcap.firebaseio.com/users");
